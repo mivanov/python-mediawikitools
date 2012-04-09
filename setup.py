6c0f2a11a9f9de165dee18580248542531fd7eb5
Mike Ivanov <mivanov@gmail.com>
2012-04-09 14:57:40 -0700

    Initial import, renamed to mediawikitools

---
+++ setup.py
@@ -0,0 +1,18 @@
+﻿#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='mediawikitools',
+      version='1.2.0',
+      description='Python package for interacting with a MediaWiki wiki',
+      long_description = """A Python package for interacting with a MediaWiki wiki using the MediaWiki API.
+Designed for MediaWiki version 1.16 and higher, should work on 1.13, older
+versions may have bugs.
+The edit-API must be enabled on the site to use editing features.
+Please report any bugs to <http://code.google.com/p/python-wikitools/issues>""",
+      author='Alex Z. (User:Mr.Z-man @ en.wikipedia)',
+      author_email='mrzmanwiki@gmail.com',
+      url='http://code.google.com/p/python-wikitools/',
+      license='GPL v3',
+      packages=['mediawikitools'],
+     )
